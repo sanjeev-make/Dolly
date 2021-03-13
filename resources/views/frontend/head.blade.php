4a7f2f00e2716a95437c3aed77be621cc1c17553
air <air@airs-MacBook-Air.local>
2021-03-14 01:51:22 +0530

    new theme add for frontend

--- resources/views/frontend/head.blade.php
+++ resources/views/frontend/head.blade.php
@@ -1,24 +1,18 @@
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title>Fortune Responsive Multipurpose Template</title>
-    <meta name="description" content="">
-
-    <!-- CSS FILES -->
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}" media="screen" data-name="skins">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/switcher.css')}}" media="screen" />
-
-        <link rel="stylesheet" href="{{asset('assets/css/fractionslider.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/style-fraction.css')}}"/>
-    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}"/>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Booksto - Responsive Bootstrap 4 Admin Dashboard Template</title>
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}" />
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+      <!-- Typography CSS -->
+      <link rel="stylesheet" href="{{asset('assets/css/typography.css')}}">
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+   </head>
--- resources/views/frontend/script.blade.php
+++ resources/views/frontend/script.blade.php
@@ -1,29 +1,53 @@
 
-<script type="text/javascript" src="{{asset('assets/js/jquery-1.10.2.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('assets/js/jquery.easing.1.3.js')}}"></script>
-<script src="{{asset('assets/js/retina-1.1.0.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.cookie.js')}}"></script> <!-- jQuery cookie -->
-<script type="text/javascript" src="{{asset('assets/js/styleswitch.js')}}"></script> <!-- Style Colors Switcher -->
-<script src="{{asset('assets/js/jquery.fractionslider.js')}}" type="text/javascript" charset="utf-8"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.smartmenus.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.smartmenus.bootstrap.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jflickrfeed.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.isotope.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.easypiechart.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/swipe.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery-scrolltofixed-min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/jquery.matchHeight-min.js')}}"></script>
-<script type="text/javascript" src="{{asset('assets/js/wow.min.js')}}"></script>
-
-<script src="{{asset('assets/js/main.js')}}"></script>
-
-<!-- Start Style Switcher -->
-<div class="switcher"></div>
-<!-- End Style Switcher -->
-<script>
-    // WOW Animation
-    new WOW().init();
-</script>
+     <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+      <script src="{{asset('assets/js/popper.min.js')}}"></script>
+      <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+      <!-- Appear JavaScript -->
+      <script src="{{asset('assets/js/jquery.appear.js')}}"></script>
+      <!-- Countdown JavaScript -->
+      <script src="{{asset('assets/js/countdown.min.js')}}"></script>
+      <!-- Counterup JavaScript -->
+      <script src="{{asset('assets/js/waypoints.min.js')}}"></script>
+      <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+      <!-- Wow JavaScript -->
+      <script src="{{asset('assets/js/wow.min.js')}}"></script>
+      <!-- Apexcharts JavaScript -->
+      <script src="{{asset('assets/js/apexcharts.js')}}"></script>
+      <!-- Slick JavaScript -->
+      <script src="{{asset('assets/js/slick.min.js')}}"></script>
+      <!-- Select2 JavaScript -->
+      <script src="{{asset('assets/js/select2.min.js')}}"></script>
+      <!-- Owl Carousel JavaScript -->
+      <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+      <!-- Magnific Popup JavaScript -->
+      <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+      <!-- Smooth Scrollbar JavaScript -->
+      <script src="{{asset('assets/js/smooth-scrollbar.js')}}"></script>
+      <!-- lottie JavaScript -->
+      <script src="{{asset('assets/js/lottie.js')}}"></script>
+      <!-- am core JavaScript -->
+      <script src="{{asset('assets/js/core.js')}}"></script>
+      <!-- am charts JavaScript -->
+      <script src="{{asset('assets/js/charts.js')}}"></script>
+      <!-- am animated JavaScript -->
+      <script src="{{asset('assets/js/animated.js')}}"></script>
+      <!-- am kelly JavaScript -->
+      <script src="{{asset('assets/js/kelly.js')}}"></script>
+      <!-- am maps JavaScript -->
+      <script src="{{asset('assets/js/maps.js')}}"></script>
+      <!-- am worldLow JavaScript -->
+      <script src="{{asset('assets/js/worldLow.js')}}"></script>
+      <!-- Raphael-min JavaScript -->
+      <script src="{{asset('assets/js/raphael-min.js')}}"></script>
+      <!-- Morris JavaScript -->
+      <script src="{{asset('assets/js/morris.js')}}"></script>
+      <!-- Morris min JavaScript -->
+      <script src="{{asset('assets/js/morris.min.js')}}"></script>
+      <!-- Flatpicker Js -->
+      <script src="{{asset('assets/js/flatpickr.js')}}"></script>
+      <!-- Style Customizer -->
+      <script src="{{asset('assets/js/style-customizer.js')}}"></script>
+      <!-- Chart Custom JavaScript -->
+      <script src="{{asset('assets/js/chart-custom.js')}}"></script>
+      <!-- Custom JavaScript -->
+      <script src="{{asset('assets/js/custom.js')}}"></script>
